1640e10184ca3ca1ff5ac150b1250c1de79c02e7
21e8 <alize_in_cryptoland@protonmail.com>
2025-01-18 19:29:54 +0100

    simple auth

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,14 +1,69 @@
 import "@fontsource/inter";
 import "@radix-ui/themes/styles.css";
 import RootLayout from "@/root-layout";
-import { client } from "@/steel-client";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Login } from "@/pages/Login";
+import { useEffect, useState } from "react";
+import { SessionsProvider } from "@/contexts/sessions-context/sessions-context";
+import { client, getSessions } from "@/steel-client";
+import { SessionContainer } from "./containers/session-container";
 
 client.setConfig({
   baseUrl: import.meta.env.VITE_API_URL,
 });
 
+type ProtectedRouteProps = {
+  children: React.ReactNode;
+};
+
+function ProtectedRoute({ children }: ProtectedRouteProps) {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const { error, data } = await getSessions();
+        if (error || !data) {
+          throw error;
+        }
+        setIsAuthenticated(true);
+      } catch (err) {
+        setIsAuthenticated(false);
+        localStorage.removeItem("auth_token");
+      }
+    };
+
+    checkAuth();
+  }, []);
+
+  if (isAuthenticated === null) {
+    // You might want to show a loading spinner here
+    return null;
+  }
+
+  return isAuthenticated ? children : <Navigate to="/login" />;
+}
+
 function App() {
-  return <RootLayout />;
+  return (
+    <SessionsProvider>
+      <RootLayout>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <SessionContainer />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      </RootLayout>
+    </SessionsProvider>
+  );
 }
 
 export default App;
--- ui/src/root-layout.tsx
+++ ui/src/root-layout.tsx
@@ -2,19 +2,38 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { QueryClientProvider } from "react-query";
 import { SessionsProvider } from "./contexts/sessions-context";
 import { queryClient } from "./lib/query-client";
-import { SessionContainer } from "./containers/session-container";
 import { Header } from "@/components/header";
 import { Toaster } from "@/components/ui/toaster";
+import { client } from "@/steel-client";
+import { Options } from "@hey-api/client-fetch";
+import { useEffect } from "react";
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  useEffect(() => {
+    const token = localStorage.getItem("auth_token");
+    const options: Options = {
+      baseUrl: import.meta.env.VITE_API_URL,
+    };
 
-export default function RootLayout() {
+    if (token) {
+      options.headers = {
+        Authorization: `Bearer ${token}`,
+      };
+    }
+    client.setConfig(options);
+    console.log("client", client);
+  }, []);
   return (
     <QueryClientProvider client={queryClient}>
       <SessionsProvider>
         <ThemeProvider defaultTheme="dark" storageKey="steel-ui-theme">
-          <div className="flex flex-col h-screen overflow-hidden max-h-screen items-center justify-center flex-1 bg-secondary text-primary-foreground">
+          <div className="flex flex-col items-center justify-center flex-1 h-screen max-h-screen overflow-hidden bg-secondary text-primary-foreground">
             <Header />
-            <div className="flex flex-col overflow-hidden flex-1 w-full">
-              <SessionContainer />
+            <div className="flex flex-col flex-1 w-full overflow-hidden">
+              {children}
             </div>
           </div>
           <Toaster />
