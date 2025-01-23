f10ec62c094b0d38a49bc5d175d21d21cc0cdf84
Andrey Novikov <envek@envek.name>
2025-01-23 18:26:27 +0900

    Allow to upload custom extensions or import from Chrome Web Store

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -2,4 +2,5 @@ export { default as actionsRoutes } from "./modules/actions/actions.routes";
 export { default as sessionsRoutes } from "./modules/sessions/sessions.routes";
 export { default as seleniumRoutes } from "./modules/selenium/selenium.routes";
 export { default as cdpRoutes } from "./modules/cdp/cdp.routes";
+export { default as extensionsRoutes} from "./modules/extensions/extensions.routes"
 export { default as filesRoutes } from "./modules/files/files.routes";
