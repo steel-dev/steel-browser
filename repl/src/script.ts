21152d7ea84cefcee3f4d1683d61b65137d15f6c
Dane Wilson <git@danewilson.me>
2025-02-13 21:52:49 +0000

    Update repl to close and edit readme

--- repl/src/script.ts
+++ repl/src/script.ts
@@ -3,17 +3,20 @@ import puppeteer from "puppeteer-core";
 async function run() {
   // WebSocket endpoint to connect Browser using Chrome DevTools Protocol (CDP)
   const wsEndpoint = "ws://0.0.0.0:3000";
-
   const browser = await puppeteer.connect({ browserWSEndpoint: wsEndpoint });
-  const page = await browser.newPage();
+  
+  try {
+    const page = await browser.newPage();
 
-  // Navigate to a website and log the title
-  await page.goto("https://steel.dev");
-  console.log(`Page title: ${await page.title()}`);
+    // Navigate to a website and log the title
+    await page.goto("https://steel.dev");
 
-  // Cleanup: close all pages and disconnect browser
-  await Promise.all((await browser.pages()).map((p) => p.close()));
-  await browser.disconnect();
+    console.log(`Page title: ${await page.title()}`);
+  } finally {
+    // Cleanup: close all pages and disconnect browser
+    await Promise.all((await browser.pages()).map((p) => p.close()));
+    await browser.disconnect();  
+  }
 }
 
 run().catch(console.error);
