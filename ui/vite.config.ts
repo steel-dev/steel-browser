30c40824a2cbaf693ec643ecc296331913e124fe
Dane Wilson <git@danewilson.me>
2025-03-31 19:23:53 +0100

    fix: also add websockets to proxy

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -17,6 +17,11 @@ export default defineConfig({
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, ""),
       },
+      "/ws": {
+        target: process.env.API_URL?.replace(/^http/, "ws"),
+        ws: true,
+        changeOrigin: true,
+      },
     },
   },
 });
