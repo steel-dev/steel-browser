b40174be6630caa70ad7899a6d55b9c920ee3a5e
Dane Wilson <git@danewilson.me>
2025-04-03 22:05:37 +0100

    Temporarily route UI to runtime ENV variable using nginx (#100)
    * feat: add nginx to docker-compose
    
    * fix: update default vars for ui API
    
    * fix: also add websockets to proxy
    
    * fix: update the targets and remove old variable
    
    * fix: renamed API_URL to distinguish between the VITE endpoint and proxy target
    
    * fix: remove build args for dockerfile and compose

--- ui/src/env.ts
+++ ui/src/env.ts
@@ -1,11 +1,8 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-  VITE_API_URL: z.string().default("http://0.0.0.0:3000"),
-  VITE_WS_URL: z.string().default("ws://0.0.0.0:3000"),
-  VITE_OPENAPI_URL: z
-    .string()
-    .default("http://0.0.0.0:3000/documentation/json"),
+  VITE_API_URL: z.string().default("/api"),
+  VITE_WS_URL: z.string().default("/ws"),
 });
 
 export const env = envSchema.parse(import.meta.env);
--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -10,4 +10,18 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, ""),
+      },
+      "/ws": {
+        target: "ws://localhost:3000",
+        ws: true,
+        changeOrigin: true,
+      },
+    },
+  },
 });
