a87b8cbf09f55b81e2f0e855b41dc51f87c69740
Dane Wilson <git@danewilson.me>
2025-04-11 20:26:42 +0100

    fix: add extra to BrowserLauncherOptions

--- api/src/types/browser.ts
+++ api/src/types/browser.ts
@@ -19,6 +19,7 @@ export interface BrowserLauncherOptions {
     height: number;
   } | null;
   userDataDir?: string;
+  extra?: Record<string, string>;
 }
 
 export interface BrowserServerOptions {
