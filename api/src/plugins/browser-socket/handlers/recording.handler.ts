be701bec7a67c23f31f5db9e54db521e77eccd46
fukouda <nasr.mohamed244@gmail.com>
2025-06-30 22:54:36 -0400

    feat: better ws handling logic + registry & options for adding custom handlers

---
+++ api/src/plugins/browser-socket/handlers/recording.handler.ts
@@ -0,0 +1,42 @@
+import { IncomingMessage } from "http";
+import { Duplex } from "stream";
+import { WebSocket } from "ws";
+import { EmitEvent } from "../../../types/enums.js";
+import { WebSocketHandler, WebSocketHandlerContext } from "../../../types/websocket.js";
+
+function handleRecordingWebSocket(context: WebSocketHandlerContext, ws: WebSocket) {
+  const { fastify } = context;
+
+  const messageHandler = (payload: { events: Record<string, any>[] }) => {
+    if (ws.readyState === WebSocket.OPEN) {
+      ws.send(JSON.stringify(payload.events));
+    }
+  };
+
+  fastify.cdpService.on(EmitEvent.Recording, messageHandler);
+
+  // TODO: handle inputs to browser from client
+  ws.on("message", async (message) => {});
+
+  ws.on("close", () => {
+    fastify.log.info("Recording WebSocket connection closed");
+    fastify.cdpService.removeListener(EmitEvent.Recording, messageHandler);
+  });
+
+  ws.on("error", (err) => {
+    fastify.log.error("Recording WebSocket error:", err);
+  });
+}
+
+export const recordingHandler: WebSocketHandler = {
+  path: "/v1/sessions/recording",
+  handler: (
+    request: IncomingMessage,
+    socket: Duplex,
+    head: Buffer,
+    context: WebSocketHandlerContext,
+  ) => {
+    context.fastify.log.info("Connecting to recording events...");
+    context.wss.handleUpgrade(request, socket, head, (ws) => handleRecordingWebSocket(context, ws));
+  },
+};
