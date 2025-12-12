55973a486fb0773e41ed7885b1d1ed7bba98f483
Dane Wilson <git@danewilson.me>
2025-12-12 13:14:07 +0000

    fix: correct type imports

--- api/src/runtime/index.ts
+++ api/src/runtime/index.ts
@@ -1,7 +1,7 @@
-export { Orchestrator, OrchestratorConfig } from "./orchestrator.js";
+export { Orchestrator, type OrchestratorConfig } from "./orchestrator.js";
 export { BrowserDriver } from "./browser-driver.js";
 export { TaskScheduler } from "./task-scheduler.js";
 
-export { createSession, CreateSessionConfig } from "./session.js";
+export { createSession, type CreateSessionConfig } from "./session.js";
 export * from "./types.js";
 export * from "./hooks.js";
